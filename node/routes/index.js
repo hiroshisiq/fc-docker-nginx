81cd2ca0fc909256e05ca511d586b6840cf24359
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-07-26 22:34:05 -0300

    feat: display people in index page

--- node/routes/index.js
+++ node/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Full Cycle Rocks!', people: ['Hiroshi', 'Siqueira'] });
 });
 
 module.exports = router;
