27ecc66f530b8eceb5fd0e476d273bcf0d7f0e8a
Hiroshi Siqueira <hiroshi.siq@gmail.com>
2021-07-26 22:30:25 -0300

    feat: create people resource

--- node/routes/people.js
+++ node/routes/people.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
+/* GET people listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+/* POST people listing. */
+router.post('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
 
 module.exports = router;
